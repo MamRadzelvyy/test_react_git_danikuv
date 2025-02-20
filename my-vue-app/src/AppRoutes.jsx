e170f72d33c5e0f9ed760377e20dab84531e154c
Jan Rylich <rylich.honza@email.cz>
2025-02-20 10:35:39 +0100

    info page

--- my-vue-app/src/AppRoutes.jsx
+++ my-vue-app/src/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Routes, BrowserRouter} from "react-router-dom"
 import Home from './pages/Home'
+import Info from './pages/Info/Info'
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/info" element={<Info />}/>
     </Routes>
     </BrowserRouter>
     </>
